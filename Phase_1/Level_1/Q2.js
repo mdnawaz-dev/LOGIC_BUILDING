1e69c01c8e3edf5fd51ea6a23c2d6f915c24ac1a
Mohammad Nawaz <nmd76158@gmail.com>
2026-09-03 16:26:57 +0530

    create Phase_1/Level_1/Q2.js

---
+++ Phase_1/Level_1/Q2.js
@@ -0,0 +1,7 @@
+let number = prompt("enter the number");
+number = Number(number);
+if (number%2==0) {
+  console.log("the given number is even");
+} else {
+  console.log("the given number is odd")
+}
