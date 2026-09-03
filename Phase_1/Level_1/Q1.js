6dfebf36e2827e1efb629c7b479a57b336f959a4
Mohammad Nawaz <nmd76158@gmail.com>
2026-09-03 16:26:46 +0530

    create Phase_1/Level_1/Q1.js

---
+++ Phase_1/Level_1/Q1.js
@@ -0,0 +1,11 @@
+let number = prompt("Enter the number:");
+number = Number(number); 
+if(number>0) {
+console.log ("Positive number");
+}
+else if (number<0) {
+console.log ("Negative number");
+}
+else {
+    console.log("it is zero");
+}
