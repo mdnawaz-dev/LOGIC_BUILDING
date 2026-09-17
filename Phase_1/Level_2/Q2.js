33856e19f51d6df0866841c7a1bff32aed8b088a
Mohammad Nawaz <nmd76158@gmail.com>
2026-09-17 06:08:30 +0530

    update Phase_1/Level_2/Q2.js

--- Phase_1/Level_2/Q2.js
+++ Phase_1/Level_2/Q2.js
@@ -1,13 +1,20 @@
-let marks = prompt("Enter your marks:");
-marks = Number(marks);
-if(marks>=90){
-  console.log("the grade is A");
-}else if(marks>=75){
-  console.log("the grade is B");
-}else if(marks>=60){
-  console.log("the grade is C");
-}else if(marks>=50){
-  console.log("the grade is D");
-}else{
-  console.log("Fail");
-};
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+  let marks = prompt("Enter your marks:");
+  marks = Number(marks);
+  if (marks >= 90) {
+    console.log("the grade is A");
+  } else if (marks >= 75) {
+    console.log("the grade is B");
+  } else if (marks >= 60) {
+    console.log("the grade is C");
+  } else if (marks >= 50) {
+    console.log("the grade is D");
+  } else {
+    console.log("Fail");
+  }
+</script>
+</body>
+</html>
